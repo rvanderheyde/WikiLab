4c52997d7dbda930fc9eece099019404c138162d
Rahil <rahil.dedhia@students.olin.edu>
2015-02-27 15:46:57 -0500

    added logout function to auth

--- routes/auth.js
+++ routes/auth.js
@@ -9,10 +9,20 @@ routes.fbAuthCallback = function(req, res){
 	res.redirect('/');
 };
 
-routes.getUsername = function(req,res){
+routes.getUsername = function(req, res){
 	var username = req.session.passport.user.displayName;
-	var obj = { userName: username};
+	var obj = {userName: username};
 	res.send(obj)
+};
+
+// Called by client when logging out to get rid of user credentials from session
+routes.loggingOut = function(req, res) {
+  // might have to change this depending on how we save things
+  req.session.passport = {};
+  req.session.userid = '';
+  
+  // send something to client to change client
+  res.send('.');
 };
 
 module.exports = routes;
