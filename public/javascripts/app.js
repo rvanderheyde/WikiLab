4763b0ba1c8efe36450179dece094d0a343a8b76
Rvanderheyde <radmer.vanderheyde@students.olin.edu>
2015-02-28 14:54:34 -0500

    nav bar custom directive done, need to move it to separate file

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -5,6 +5,43 @@
 		$http.get()
 	}]);
 
+	app.directive('navBar', ['$cookieStore', '$http', function($cookieStore, $http){
+		return {
+			restrict: 'E',
+			templateUrl: '../templates/nav.html',
+			controller: function(){
+				var user = this;
+				this.username = '';
+
+				$http.get('/session/username').success(function(data){
+					//bake the cookie with username from server to control view.
+					if (data !== 'error'){
+						console.log(data);
+						var username = data.userName;
+						$cookieStore.put('username', username);
+						user.username = username;
+					}
+				}).error(function(data){
+					alert(data);	
+				});
+
+
+				this.eatCookie = function(){
+					//eat the cookie!!(destroys it)
+					var username = $cookieStore.get('username');
+					$http.post('/session/end').success(function(data, status, headers, config){
+						console.log(username);
+						$cookieStore.remove('username');
+						user.username = '';
+					}).error(function(data,status,headers,config){
+						alert("There was an err loggin out")
+					})
+				};
+			},
+			controllerAs:'nav'
+		};
+	}]);
+
 	app.controller('NavController',['$cookieStore', '$http','$scope', function($cookieStore, $http, $scope){
 		var user = this;
 		this.username = '';
