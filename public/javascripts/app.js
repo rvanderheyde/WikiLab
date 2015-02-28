d3075076ef53d82750ca1a92f58127c607aeaccf
Rvanderheyde <radmer.vanderheyde@students.olin.edu>
2015-02-28 14:25:51 -0500

    fb stuff finished, logout works, login works, view changes when both happen

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -10,6 +10,7 @@
 		this.username = '';
 
 		$http.get('/session/username').success(function(data){
+			//bake the cookie with username from server to control view.
 			if (data !== 'error'){
 				console.log(data);
 				var username = data.userName;
@@ -22,10 +23,13 @@
 
 
 		this.eatCookie = function(){
+			//eat the cookie!!(destroys it)
 			var username = $cookieStore.get('username');
 			$http.post('/session/end').success(function(data, status, headers, config){
+				console.log(username);
 				$cookieStore.remove('username');
 				$scope.check = false;
+				user.username = '';
 			}).error(function(data,status,headers,config){
 				alert("There was an err loggin out")
 			})
