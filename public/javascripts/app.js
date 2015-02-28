a85d86bb4bfdc3dcd428570c82123308c235d093
Rvanderheyde <radmer.vanderheyde@students.olin.edu>
2015-02-28 13:32:55 -0500

    fixed stupid controller ship

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -5,7 +5,7 @@
 		$http.get()
 	}]);
 
-	app.controller('NavController',['$cookieStore', '$http','$scope', function($scope, $cookieStore, $http){
+	app.controller('NavController',['$cookieStore', '$http','$scope', function($cookieStore, $http, $scope){
 		this.checksum = 0;
 		$scope.username = 'test';
 
@@ -17,7 +17,7 @@
 						if (data !=='No User'){
 							var username = data.userName;
 							$cookieStore.put('username', username);
-							console.log(1 + $cookieStore.get('username'))
+							console.log(2 + $cookieStore.get('username'))
 							$scope.username = username;
 							// this.check = true;
 							return true;
@@ -29,13 +29,25 @@
 					});
 				} else if($cookieStore.get('username')){
 					// this.check = true;
+					console.log($cookieStore.get('username'));
 					return true;
 				} else {
 					// this.check = false;
+					console.log('WAT!');
 					return false;
 				}
-			} else { console.log('done'); }
+			} else { console.log('done'); return $scope.check}
 		};
+
+		this.eatCookie = function(){
+			var username = $cookieStore.get('username');
+			$http.post('/session/end').success(function(data, status, headers, config){
+				$cookieStore.remove('username');
+				$scope.check = false;
+			}).error(function(data,status,headers,config){
+				alert("There was an err loggin out")
+			})
+		}
 		$scope.check = this.bakeCookie();
 			
 	}]);
