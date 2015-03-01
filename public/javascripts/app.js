b2b398082b50c711a8645660a2f604003da94f61
Rahil <rahil.dedhia@students.olin.edu>
2015-03-01 13:53:04 -0500

    but actually resolved merge conflicts now

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -14,28 +14,6 @@
 
     $locationProvider.html5Mode(true);
   }]);
-  
-  app.controller('LinkController', ['$http', '$location', function($http, $location){
-    var links = this;
-    this.paths = [];
-
-    $http.get('/db/pages').success(function(data, status){
-      links.paths = data.pages;
-    }).error(function(data, status){
-      alert(status, data);
-    })
-
-    this.checkPage = function(){
-      var curPath = $location.path();
-      if (curPath.length < 2){
-        return true;
-      } else if (curPath === '/_=_') {
-        return true;
-      } else {
-        return false
-      }
-    }
-  }]);
 
 	app.controller('LinkController', ['$http', '$location', function($http, $location){
 		var links = this;
