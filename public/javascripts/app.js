ae4154c7298b71a75b1f778320e7eea5be221eaa
Rvanderheyde <radmer.vanderheyde@students.olin.edu>
2015-03-01 14:17:52 -0500

    fix rahils shit

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -18,7 +18,27 @@
     return {
       restrict: 'E',
       templateUrl: 'linkList.html',
-      controller:
+      controller: function(){
+        var links = this;
+        this.paths = [];
+
+        $http.get('/db/pages').success(function(data, status){
+          links.paths = data.pages;
+        }).error(function(data, status){
+          alert(status, data);
+        })
+
+        this.checkPage = function(){
+          var curPath = $location.path();
+          if (curPath.length < 2){
+            return true;
+          } else if (curPath === '/_=_') {
+            return true;
+          } else {
+            return false
+          }
+        }
+      },
       controllerAs: 
     };
   }]);
