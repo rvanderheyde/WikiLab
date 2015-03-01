7456cdc75bbbbf51dbec325ffb96489380081988
Rahil <rahil.dedhia@students.olin.edu>
2015-03-01 13:52:24 -0500

    resolved merge conflicts

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -37,13 +37,43 @@
     }
   }]);
 
-  app.controller('BodyController', ['$cookieStore', '$http', '$location', function($cookieStore, $http, $location) {
+	app.controller('LinkController', ['$http', '$location', function($http, $location){
+		var links = this;
+		this.paths = [];
+
+		$http.get('/db/pages').success(function(data, status){
+			links.paths = data.pages;
+			console.log(links.paths)
+		}).error(function(data, status){
+			alert(status, data);
+		})
+
+		this.goTo = function(path){
+			$http.get(path).success(function(data, status){
+				$location.path(path);
+				var content = data;
+			}).error(function(data,status){
+				alert(status);
+			});
+		};
+
+		this.checkPage = function(){
+			var curPath = $location.path();
+			if (curPath.length > 1){
+				return false;
+			} else {
+				return true;
+			}
+		}
+	}]);
+
+  app.controller('BodyController', ['$cookieStore', '$http', function($cookieStore, $http) {
     this.page = {};
     this.makePost = function () {
       data = this.page;
       data.user = $cookieStore.get('username');
       // this is probably a stupid way to make the form blank
-      // $('#new_page').find('.blank').val('');
+      $('#new_page').find('.blank').val('');
       this.page = {};
 
       $http.post('newPost', data)
